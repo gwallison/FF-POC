d967a1efdf77cc200c3bbbc36c3283819ca74210
allisong <gary.allison@gmail.com>
2019-09-26 05:56:46 -0400

    saving data sets to disk

--- build_database.py
+++ build_database.py
@@ -59,23 +59,28 @@ raw_stats_fn = outdir+'ff_raw_stats.txt'
 
 #### ----------    end File Handles ----------  ####
 
-raw_df = rff.Read_FF(zname=zfilename,make_pickle=make_pickle,
-                     picklefn=raw_pickle_fn).import_raw()
-if rerun_raw_stats:
-    ffstats.FF_stats(raw_df,outfn=raw_stats_fn).calculate_all()
-raw_df = raw_df[keep_col].copy()
-df = parse_raw.Parse_raw().clean_fields(raw_df)
-raw_df = None  # we are done with this monster, get it out of memory
-df = flag_ev.Flag_events().clean_events(df)
-df = cat_rec.Categorize_CAS(df=df,sources=sources,outdir=outdir).do_all()
-df = abc.Add_bg_columns(df,sources=sources).add_all_cols()
-df = proc_mass.Process_mass(df).run()    
-if make_pickle:
-    print('Pickling full data set')
-    df.to_pickle(full_pickle_fn)
-    
 # =============================================================================
-# else:
-#     df = pd.read_pickle(full_pickle_fn)
-# 
+# raw_df = rff.Read_FF(zname=zfilename,make_pickle=make_pickle,
+#                      picklefn=raw_pickle_fn).import_raw()
+# if rerun_raw_stats:
+#     ffstats.FF_stats(raw_df,outfn=raw_stats_fn).calculate_all()
+# raw_df = raw_df[keep_col].copy()
+# df = parse_raw.Parse_raw().clean_fields(raw_df)
+# raw_df = None  # we are done with this monster, get it out of memory
+# df = flag_ev.Flag_events().clean_events(df)
+# df = cat_rec.Categorize_CAS(df=df,sources=sources,outdir=outdir).do_all()
+# df = abc.Add_bg_columns(df,sources=sources).add_all_cols()
+# df = proc_mass.Process_mass(df).run()    
+# if make_pickle:
+#     print('Pickling full data set')
+#     df.to_pickle(full_pickle_fn)
+#     
 # =============================================================================
+
+df = pd.read_pickle(full_pickle_fn)
+import core.Make_working_set as mws
+mws.save_master_df(df)
+df = mws.get_filtered_df(df)
+import numpy as np
+df['lgMass'] = np.log10(df.bgMass+0.0001)
+df[df.bgCAS=='7732-18-5'].lgMass.hist()
---
+++ core/Make_working_set.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 12:04:34 2019
+
+@author: Gary
+
+This module is used to store the master dataframe and 
+create a new dataframe that has only cleaned data
+and store it in the output directory as well as return it.
+"""
+
+def save_master_df(df,outdir='./out/'):
+    df.to_csv(outdir+'full_df.csv')
+    
+def save_single_state(df,outdir='./out/',state='ohio'): 
+    """state must be lower case"""
+    df[df.bgStateName==state].to_csv(outdir+state+'_only.csv')
+    
+def get_filtered_df(df,outdir = './out/',
+                    keepcodes = 'M',removecodes= 'R|1|2|4|5',all_cols=False,
+                    col_list=['UploadKey','CASNumber','IngredientName','Purpose','OperatorName',
+                               'Supplier','MassIngredient','PercentHFJob','DQ_code',
+                               'bgCAS','bgIngredientName','bgMass','JobStartDate','date','StateName','api10',
+                               'bgSupplier','bgStateName','bgOperatorName']):
+    """return filtered dataset, optionally with a subset of columns"""
+    print('Creating filtered version of data set')
+    if all_cols: df = df.copy()
+    else: df = df[col_list].copy()
+    print(f'Initial length:           {len(df)}')
+    if keepcodes: df = df[df.DQ_code.str.contains(keepcodes)]
+    print(f'  after keepcodes({keepcodes}):   {len(df)}')
+    if removecodes: df = df[~df.DQ_code.str.contains(removecodes)]
+    print(f'  after removecodes ({removecodes}): {len(df)}')
+    df.to_csv(outdir+'filtered_df.csv')
+    return df.copy()
